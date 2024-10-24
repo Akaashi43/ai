80094d1ac5eeade23adbf74d621055f8eb770414
@_Akash_:) <118038514+Akaashi43@users.noreply.github.com>
2024-10-25 00:41:00 +0530

    Update prac2-Astar.py

--- prac2-Astar.py
+++ prac2-Astar.py
@@ -64,26 +64,17 @@ def main():
 main()
 
 """
-OUTPUT:
+# OUTPUT:
 The A* path with the total is: 
 Arad , Sibiu , Rimnicu , Pitesti , Bucharest : : 418
+####
 
- What is an A* algorithm?
- It is a searching algorithm that is used to find the shortest path
- between an initial and a final point.
- It is a handy algorithm that is often used for map traversal to find the
- shortest path to be taken. A* was initially designed as a graph
- traversal problem, to help build a robot that can find its own course. It
- still remains a widely popular algorithm for graph traversal.
- It searches for shorter paths first, thus making it an optimal and
- complete algorithm. An optimal algorithm will find the least cost
- outcome for a problem, while a complete algorithm finds all the
- possible outcomes of a problem.
- Another aspect that makes A* so powerful is the use of weighted
- graphs in its implementation. A weighted graph uses numbers to
- represent the cost of taking each path or course of action. This means
- that the algorithms can take the path with the least cost, and find the
- best route in terms of distance and time
+What is A* Search Algorithm? 
+A* Search algorithm is one of the best and popular technique used in path-finding and graph traversals.
+
+Why A* Search Algorithm? 
+Informally speaking, A* Search algorithms, unlike other traversal techniques, it has “brains”. What it means is that it is really a smart algorithm which separates it from the other conventional algorithms. This fact is cleared in detail in below sections. 
+And it is also worth mentioning that many games and web-based maps use this algorithm to find the shortest path very efficiently (approximation).
 """
 
         
